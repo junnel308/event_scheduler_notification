c587e30cf0a3f0ef33d15f01bdf2e0e45fc5f9ed
junnel308 <junnelpadilla08@gmail.com>
2022-10-15 16:39:13 -0700

    organized HTML code and CSS code and fixed a bug when button goes to the next line when resizing the screen and not aligned with the input log and the time display

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 var todaysDate = document.querySelector("#todays-date");
+var lockButton = document.querySelector('button');
 
 var date = new Date();
+var currentHour = date.getHours();
 var currentDate = date.toDateString();
 
 todaysDate.value = currentDate;
 
+if(currentHour) { 
+    displayHere.value = "Current Hour";
+    displayHere.style.backgroundColor = "blue";
+}
+
+
+function loggingEvents () {
+    var logIt = displayEventHere();
+    var displayHere = document.querySelector('input');
+
+    displayHere.value = logIt;
+}
+
+function displayEventHere () {
+    var resultDisplay = document.createElement('h2');
+    resultDisplay = "Event Saved"
+    resultDisplay.parentElement(resultDisplay);
+    document.displayHere.style.color = "blue";
+}
+
+lockButton.addEventListener("click", loggingEvents);
